bc502ecbd509efe825da7b63651d1a3cc528e570
Taruna Chaudhary <taruna.chaudhary@routerarchitects.com>
2025-10-14 18:14:35 +0530

    Added License header

--- src/RESTAPI/RESTAPI_inventory_handler.cpp
+++ src/RESTAPI/RESTAPI_inventory_handler.cpp
@@ -1,3 +1,9 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0 OR LicenseRef-Commercial
+ * Copyright (c) 2025 Infernet Systems Pvt Ltd
+ * Portions copyright (c) Telecom Infra Project (TIP), BSD-3-Clause
+ */
+
 //
 //	License type: BSD 3-Clause License
 //	License copy: https://github.com/Telecominfraproject/wlan-cloud-ucentralgw/blob/master/LICENSE
