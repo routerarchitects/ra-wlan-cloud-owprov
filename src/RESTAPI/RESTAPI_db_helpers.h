8ef6ab6a0152787d546757b4bdc26390e1c7d4e8
Taruna Chaudhary <taruna.chaudhary@routerarchitects.com>
2026-01-22 16:20:26 +0530

    [GET-INVENTORY] Add subscriber to extendedInfo
    Fix Type: Feature
    Root Cause: GET /api/v1/inventory?withExtendedInfo=true omitted subscriber details needed by UI.
    Solution: Include subscriber info in extendedInfo using signup data.

--- src/RESTAPI/RESTAPI_db_helpers.h
+++ src/RESTAPI/RESTAPI_db_helpers.h
@@ -68,6 +68,25 @@ namespace OpenWifi {
 	}
 	template <typename... Ts> void Extend_venue(Ts... args) { static_assert(sizeof...(args) == 2); }
 
+	template <typename R, typename Q = decltype(R{}.subscriber)>
+	void Extend_subscriber(const R &T, Poco::JSON::Object &EI) {
+		if constexpr (std::is_same_v<Q, std::string>) {
+			if (!T.subscriber.empty()) {
+				Poco::JSON::Object SubObj;
+				ProvObjects::SignupEntry Signup;
+				if (StorageService()->SignupDB().GetRecord("userid", T.subscriber, Signup)) {
+					SubObj.set("email", Signup.email);
+					SubObj.set("description", Signup.info.description);
+				}
+				SubObj.set("id", T.subscriber);
+				EI.set("subscriber", SubObj);
+			}
+		}
+	}
+	template <typename... Ts> void Extend_subscriber(Ts... args) {
+		static_assert(sizeof...(args) == 2);
+	}
+
 	template <typename R, typename Q = decltype(R{}.contact)>
 	void Extend_contact(const R &T, Poco::JSON::Object &EI) {
 		if constexpr (std::is_same_v<Q, std::string>) {
@@ -142,6 +161,7 @@ namespace OpenWifi {
 		Extend_location(T, EI);
 		Extend_contact(T, EI);
 		Extend_venue(T, EI);
+		Extend_subscriber(T, EI);
 		Extend_managementPolicy(T, EI);
 		O.set("extendedInfo", EI);
 		return true;
