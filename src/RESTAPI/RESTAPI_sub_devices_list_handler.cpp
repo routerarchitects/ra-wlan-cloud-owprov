16d650cfc90c3179312d29b9db3e408c88c92830
Anil-routerArch <anil24.behera@routerarchitects.com>
2026-08-04 12:22:49 +0530

    refactor(subscriberDevice): query strictly by operatorId using ListHandlerForOperator

--- src/RESTAPI/RESTAPI_sub_devices_list_handler.cpp
+++ src/RESTAPI/RESTAPI_sub_devices_list_handler.cpp
@@ -15,33 +15,6 @@ namespace OpenWifi {
 			return BadRequest(RESTAPI::Errors::OperatorIdMustExist);
 		}
 
-		if (!operatorId.empty() && subscriberId.empty()) {
-			std::string operatorEntityId = operatorId;
-			ProvObjects::Entity E;
-			if (StorageService()->EntityDB().GetRecord("operatorId", operatorId, E)) {
-				operatorEntityId = E.info.id;
-			} else if (StorageService()->EntityDB().GetRecord("id", operatorId, E)) {
-				operatorEntityId = E.info.id;
-			}
-
-			std::string WhereClause = fmt::format(
-				" operatorId='{}' or operatorId='{}' or subscriberId in (select subscriber from venues where subscriber!='' and (entity='{}' or entity='{}')) ",
-				operatorId, operatorEntityId, operatorEntityId, operatorId);
-
-			if (QB_.CountOnly) {
-				auto Count = DB_.Count(WhereClause);
-				return ReturnCountOnly(Count);
-			}
-
-			if (!QB_.Select.empty()) {
-				return ReturnRecordList<SubscriberDeviceDB, ProvObjects::SubscriberDevice>("subscriberDevices", DB_, *this);
-			}
-
-			SubscriberDeviceDB::RecordVec Entries;
-			DB_.GetRecords(QB_.Offset, QB_.Limit, Entries, WhereClause);
-			return MakeJSONObjectArray("subscriberDevices", Entries, *this);
-		}
-
 		return ListHandlerForOperator<SubscriberDeviceDB>("subscriberDevices", DB_, *this,
 														  operatorId, subscriberId);
 	}
